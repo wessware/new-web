4ea0163e02b2e52dcfe990d19ee09d8b734bc880
wessware <stevenshisia@yahoo.com>
2020-09-20 10:12:58 +0300

    Multiple changes to improve user experience.

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -1,24 +1,20 @@
 <?php
-$myemail = "setkenya@gmail.com"; 
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-if (strlen($name) < 2) {
-    $error['name'] = "Please re-enter a valid name";
-}
-if(!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is'), $email) {
-    $error['email'] = "Please enter a valid email address";
-}
-if(strlen($message) < 15) {
-    $error['message'] = "Please re-enter your message. It should have at least 15 characters."
-}
+$formcontent=" From: $name \n Phone: $phone \n Mail: $email \n Message: $message";
 
-$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
-$msg = wordwrap($msg,70);
-mail($myemail,"New Form Submission",$msg);
-mail($email,"Thank you for your Feedback",$msg);
-echo 'Set Kenya values your Feedback. Thank you for your Contribution.  <a href="index.html">Click here to return to our homepage.</a>';
+$recipient = "setkenya@gmail.com";
+
+$subject = "Contact Form";
+
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You for your Feedback on Set Kenya. \n 
+We will use it to the best of effect. Follow this link to return to our <a href="index.html">home page"</a>;
+
 ?>
